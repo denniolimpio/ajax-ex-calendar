4f0162c1d018ed9564f3217791a92be7a498c685
dennis <dennis.olimpio0794@gmail.com>
2020-06-30 17:51:07 +0200

    correzione  1/2

--- js/script.js
+++ js/script.js
@@ -9,16 +9,138 @@ $(document).ready( function() {
   // Sarà indispensabile sia per la parte obbligatoria, che per quella facoltativa,
   // l’utilizzo di momentjs e dell’API holiday
 
+
+// rappresento il primo MESE utilizzando un oggetto
+
+var dataIniziale = moment( {
+
+  day:  1,
+  month: 0, // 0 = gennaio , 1 = febbraio (..)
+  year: 2018,
+
+
+});
+
+// invoco la funzione per stampare tutti i giorni del mese
+
+stampoGiorniMese(dataIniziale);
+
+// invoca la funzione che aggiunge le festività
+
+stampoGiorniFestivita(dataIniziale);
+
+
+
+// ---- FUNZIONI -----
+
+
+
+//  ------------   #1 stampoGiorniMese  ------------
+
+function stampoGiorniMese (dataIniziale) {
+
+
+  // Rendo il titolo dinamico
+
+  $("h1").text(dataIniziale.format("MMMM YYYY"))
+
+  // daysInMonth() calcola in automatico il numero di giorni di cui è composto il mese
+
+  var giorniMese =  dataIniziale.daysInMonth();
+
+  //
+  // console.log(dataIniziale);
+  // console.log('giorni del mese ' + giorniMese);
+
+
+  //  stampo i giorni del mese utilizzando handlebars
+
+  var source = $("#calendar-template").html() // utilizzo jquery
+  var template = Handlebars.compile(source);
+
+
+// inizio ciclo for
+  for ( var i = 1; i <= giorniMese; i++ ) {
+
+    var giornoCorrente = moment( {
+
+      day: i,
+      month: dataIniziale.month(),
+      year: dataIniziale.year()
+
+    })
+
+    // console.log(giornoCorrente);
+
+
+  // vado a riempire il template
+
+    var context = {
+
+      date: giornoCorrente.format( "D MMMM "),
+      giorno_attr: giornoCorrente.format( "YYYY - MM - DD")
+
+     };
+
+
+
+    var html = template(context);
+    // console.log(i);
+
+  $("#listaGiorni").append(html);
+
+  }
+  // fine  ciclo for
+
+}
+
+
+
+
+//  ------------   #2 stampoGiorniFestivita  ------------
+
+
+function stampoGiorniFestivita(dataInziale) {
+
+  // Chiamo l'api con le festività
+  // Aggiungo la classe ".festivita" alla data cui corrisponde una festività
+
+
   $.ajax (
 
     {
       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
       method: "GET",
+      data: {
+
+        year: dataInziale.year(),
+        month: dataInziale.month()
+
+
+      },
 
       // se l'api viene caricato correttamente
-      success: function(data) {
+      success: function(dataResponse) {
 
-        console.log(data.response);
+        // console.log(dataResponse);
+
+        var holidays = dataResponse.response;
+
+
+        for (var i = 0; i < holidays.length; i++) {
+
+          var currentHolidays = holidays[i];
+          console.log(currentHolidays);
+
+        // $(".giorno")
+
+
+
+
+          });
+
+        }
+
 
       },
       // se l'api NON viene caricato correttamente
@@ -30,50 +152,8 @@ $(document).ready( function() {
       }
 
 
-    })
-
-// FORMATO DATA (ANNO - MESE - GIORNO)
-
-
-var start = moment("2018-01-01")
-var giornoMese = moment(2).daysInMonth();
-
-var day = start.format("D");
-var month = start.format("MMMM");
-var year = start.format("YYYY");
-
-var span = $("span");
-
-$(".anno").children(span).append(year);
-
-$(".mese").children(span).append(month);
-
-
-
-for (var i = 0; i <= giornoMese; i++ ) {
-
-  $(".giorno > .giorniMese").append(" <li> " + [i + 1] + " </li>");
-
-
+    });
 }
-
-
-console.log(year)
-console.log(day)
-console.log(month)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
